72a3ab2fd55a632f1528c121b89c421fb2a1b09b
Hyeokwoo Alex Kwon <khwalex@gmail.com>
2019-03-03 12:40:03 +0900

    edit component

--- Lecture3/pokedex/src/components/GalleryCarousel/index.js
+++ Lecture3/pokedex/src/components/GalleryCarousel/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const GalleryCarousel = ({ images, onSelect }) => {
+const GalleryCarousel = () => {
   return (
     <Wrapper>
     </Wrapper>
